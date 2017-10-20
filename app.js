a4e0334c32f6b029721b266a2754f6ecc6f95fc7
Vinicius Pontes <vinicius.pontes@cubos.io>
2017-10-20 04:02:07 -0200

    Added views

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 var express = require('express');
 var app = express();
 
+app.set('view engine', 'ejs');
+
 app.get('/', function(req, res){
     res.end("<html><body><h4>Teste1</h4></html></body>");
 })
-
+app.get('tech', function(req, res){
+    res.render('section/tech');
+})
 app.listen(3000, function(){
-    console.log('Server on')
+    console.log('Server on');
 })
